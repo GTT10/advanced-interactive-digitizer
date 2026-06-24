5ef32e153083bef5b188fbad18756f3b7a9376d4
Toshiki Kakeya <pqg693x1@s.okayama-u.ac.jp>
2026-06-25 04:25:57 +0900

    Avoid eager OpenCV import from package root

--- src/aidigitizer/__init__.py
+++ src/aidigitizer/__init__.py
@@ -1,17 +1,13 @@
 """Advanced Interactive Digitizer package."""
 
-from aidigitizer.axis_detection import AxisDetectionResult, LineSegment, detect_axes
 from aidigitizer.calibration import AxisCalibration, AxisScale, PlotCalibration
 from aidigitizer.models import DataSeries, DigitizedPoint, DigitizerProject
 
 __all__ = [
     "AxisCalibration",
-    "AxisDetectionResult",
     "AxisScale",
     "DataSeries",
     "DigitizedPoint",
     "DigitizerProject",
-    "LineSegment",
     "PlotCalibration",
-    "detect_axes",
 ]
