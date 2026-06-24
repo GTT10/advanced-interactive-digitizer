8b61ecec1090d26df7058f25d6e410ffc936374c
Toshiki Kakeya <pqg693x1@s.okayama-u.ac.jp>
2026-06-25 04:15:31 +0900

    Export axis detection helpers

--- src/aidigitizer/__init__.py
+++ src/aidigitizer/__init__.py
@@ -1,13 +1,17 @@
 """Advanced Interactive Digitizer package."""
 
+from aidigitizer.axis_detection import AxisDetectionResult, LineSegment, detect_axes
 from aidigitizer.calibration import AxisCalibration, AxisScale, PlotCalibration
 from aidigitizer.models import DataSeries, DigitizedPoint, DigitizerProject
 
 __all__ = [
     "AxisCalibration",
+    "AxisDetectionResult",
     "AxisScale",
-    "PlotCalibration",
     "DataSeries",
     "DigitizedPoint",
     "DigitizerProject",
+    "LineSegment",
+    "PlotCalibration",
+    "detect_axes",
 ]
